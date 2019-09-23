6b5855d721e62895e6ad4487c076251fd1d6fa50
Clay <Clay@10.0.64.29>
2019-09-23 11:57:08 -0500

    the background is now the color of the motherland's blood

--- UCSDUnfoldingMaps/src/module1/HelloWorld.java
+++ UCSDUnfoldingMaps/src/module1/HelloWorld.java
@@ -43,7 +43,7 @@ public class HelloWorld extends PApplet
 
 		// This sets the background color for the Applet.  
 		// Play around with these numbers and see what happens!
-		this.background(140, 175, 255);
+		this.background(255, 50, 50);
 		
 		// Select a map provider
 		AbstractMapProvider provider = new Google.GoogleTerrainProvider();
@@ -66,11 +66,11 @@ public class HelloWorld extends PApplet
 		// There are several providers built-in.
 		// Note if you are working offline you must use the MBTilesMapProvider
 		map1 = new UnfoldingMap(this, 50, 50, 350, 500, provider);
-		map2 = new UnfoldingMap(this, 450, 50, 350, 500, provider);
+		map2 = new UnfoldingMap(this, 450, 50, 300, 500, provider);
 		// The next line zooms in and centers the map at 
 	    // 32.9 (latitude) and -117.2 (longitude)
 	    map1.zoomAndPanTo(zoomLevel, new Location(32.9f, -117.2f));
-		map2.zoomAndPanTo(zoomLevel, new Location(36.0f, -110.2f));
+		map2.zoomAndPanTo(zoomLevel, new Location(48.7f, 44.4f));
 		// This line makes the map interactive
 		MapUtils.createDefaultEventDispatcher(this, map1);
 		MapUtils.createDefaultEventDispatcher(this, map2);
